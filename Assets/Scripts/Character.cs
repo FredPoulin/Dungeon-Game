5be87f7da4e9b121cc01180864a995f5b6e680cc
FredPoulin <frederick.poulin@hotmail.com>
2017-12-05 15:36:12 -0500

    Player movement
    Added a movment system to the player character

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour {
+
+    [SerializeField]
+    private float speed;
+
+    protected Vector2 direction;
+
+    [SerializeField]
+    private Animator animator;
+
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	protected virtual void Update () {
+        Move();
+    }
+
+    public void Move()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+
+        if (direction.x != 0 || direction.y!=0)
+        {
+            AnimateMovement();
+        }
+        else
+        {
+            animator.SetLayerWeight(1, 0);
+        }
+        
+    }
+
+    public void AnimateMovement()
+    {
+        animator.SetLayerWeight(1, 1);
+
+        animator.SetFloat("X", direction.x);
+        animator.SetFloat("Y", direction.y);
+    }
+}
